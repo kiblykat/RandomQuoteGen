d6e5120e69482fc078e30f002e4fae6acaa7db16
izzat <izzatfadzlon@gmail.com>
2023-09-10 23:42:20 +0800

    Added ability to get request advice from web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends Component {
+    state = {advice: ''}
+    
+    componentDidMount() {
+        this.fetchAdvice()
+    }
+
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response)=>{
+                this.setState({
+                    advice: response.data.slip.advice
+                }) 
+            })
+            .catch(error =>{
+                console.log(error)
+            })       
+    }    
+
+    render() {
+        return (
+            <h1>{this.state.advice}</h1>
+        )
+    }
 }
 
-export default App;
+
+export default App
